aebcaa6b23c2311f6443996e151305698ed800d5
andrean.buhchev <andeeowl@yahoo.com>
2022-09-28 13:23:10 +0300

    splitted tests into separate  test cases

--- _tests_/buttonPressHandler.spec.js
+++ _tests_/buttonPressHandler.spec.js
@@ -1,11 +1,19 @@
 import { buttonPressHandler } from "../services/buttonPressHandler";
 
 describe("Handle button presses", () => {
-  test("should return the right values from given input", () => {
+  test("should return the right values when enter 1 and text is 0", () => {
     expect(buttonPressHandler("1", "0", "0")).toEqual(["1"]);
+  });
+  test("should return the right values when enter + and text is 0", () => {
     expect(buttonPressHandler("+", "0", "0")).toEqual(["0+"]);
+  });
+  test("should return the right values when enter + and text is 1+", () => {
     expect(buttonPressHandler("+", "1+", "+")).toEqual(["1+"]);
+  });
+  test("should return the right values when enter 1 and text is 1+", () => {
     expect(buttonPressHandler("1", "1+", "+")).toEqual(["1+1"]);
+  });
+  test("should return the right values when enter + and text is 1+1 ", () => {
     expect(buttonPressHandler("+", "1+1", "1")).toEqual([2, "2+"]);
   });
 });
--- _tests_/operationPressHandler.spec.js
+++ _tests_/operationPressHandler.spec.js
@@ -1,11 +1,19 @@
 import { operationPressHandler } from "../services/operationPressHandler";
 
 describe("Handle operation presses", () => {
-  test("should return the right values from given input", () => {
+  test("should delete last char when entered DEL", () => {
     expect(operationPressHandler("11", "DEL", "1")).toEqual(["1"]);
+  });
+  test("should return two 0 values when entered AC", () => {
     expect(operationPressHandler("11", "AC", "1")).toEqual(["0", "0"]);
+  });
+  test("should return the right values when entered =", () => {
     expect(operationPressHandler("1+1", "=", "1")).toEqual([2, 2]);
+  });
+  test("should do nothing if entered = and last char is operator", () => {
     expect(operationPressHandler("1+", "=", "+")).toEqual();
+  });
+  test("should return 0 if text lenght is 1", () => {
     expect(operationPressHandler("1", "DEL", "1")).toEqual(["0"]);
   });
 });
